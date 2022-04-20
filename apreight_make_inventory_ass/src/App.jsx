fd28ec664c5a0301a5241cc66cd65b4422c3f2e4
sashanka1 <sashankasekharsamal777@gmail.com>
2022-04-21 02:35:32 +0530

    done

---
+++ apreight_make_inventory_ass/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Inventory } from "./component/inve";
+
+function App() {
+  return (
+    <div className="App">
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
