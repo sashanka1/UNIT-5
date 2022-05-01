c0f2ea61cc49ac6da74720591881ba66527bb008
sashanka1 <sashankasekharsamal777@gmail.com>
2022-05-01 12:36:11 +0530

    done

---
+++ googletimerass/src/App.js
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+        <div className="styl">
+      <Counter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
