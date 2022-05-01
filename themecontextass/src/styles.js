379acd61d467c6cb36d96f8f29c81a9f5f90992a
sashanka1 <sashankasekharsamal777@gmail.com>
2022-05-01 13:08:45 +0530

    done

---
+++ themecontextass/src/styles.js
@@ -0,0 +1,13 @@
+export const themeModes = {
+    dark: {
+      backgroundColor: "yellow",
+      color: "black",
+      borderRadious:"10px",
+      border: "5px solid blue",
+    },
+    light: {
+      backgroundColor: "white",
+      color: "black",
+      border: "5px solid black",
+    },
+  };
